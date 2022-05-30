df512dbfc2a9ff87b597cb5bcaf2c2ddc2da4f24
maany <maany@LAPTOP-RI0J09NV>
2022-05-30 19:25:17 +0530

    First commit

--- CoreJava/src/Projects/FinalDemo.java
+++ CoreJava/src/Projects/FinalDemo.java
@@ -1,17 +1,17 @@
 package Projects;
 
 public class FinalDemo {
-	final int speedlimit=90;
-	void run() {
-		//speedlimit = 50;
-	}
+	final int speedlimit=90;//Final variable
+	//void run() {
+	//speedlimit = 50;
+	//}
 	
-
+//void run gives error bcoz Final value cannot be changed
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		FinalDemo fd = new FinalDemo();
-		fd.run();
-
+		//FinalDemo fd = new FinalDemo();
+		//fd.run();
+		
 	}
 
 }
--- CoreJava/src/Projects/Library.java
+++ CoreJava/src/Projects/Library.java
@@ -2,27 +2,44 @@ package Projects;
 import java.util.Scanner;
 
 public class Library {
-	int AccNum;
+	int Acc_Num;
 	String Title;
 	String Author;
-	void input( ) {
+public void input( )
+{
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter accession number:");
-		AccNum = sc.nextInt();
-		System.out.println("Enter Book Title:");
-		Title = sc.nextLine();
+		System.out.println("Enter the Title");
+		Title= sc.nextLine();
+		System.out.println();
 		
-		System.out.println("Enter Book Author:");
+		
+		System.out.println("Enter the Author name");
 		Author = sc.nextLine();
+		System.out.println("Enter the account number");
+		Acc_Num = sc.nextInt();
 		
 	}
-	void compute()
+public void compute()
 	{
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the number of days late:");
+	int days = sc.nextInt();
+	int fine = (days*5);
+	System.out.println("Fine = Rs:"+fine );
 		
 	}
+public void display()
+{
+	System.out.println("Acc_Num\tTitle\tAuthor");
+	System.out.println(Acc_Num + "\t"+Title+"\t" +Author);
+}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Library lib = new Library();
+		lib.input();
+		lib.compute();
+		lib.display();
 
 	}
 
